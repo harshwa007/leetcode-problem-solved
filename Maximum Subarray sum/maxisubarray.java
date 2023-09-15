28d171eb5c8c4a7297dce03a2c1395cd8ace9340
harshwa007 <harshwa007@gmail.com>
2023-09-15 22:32:48 +0530

    Uploaded

---
+++ Maximum Subarray sum/maxisubarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        int minInt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+}
