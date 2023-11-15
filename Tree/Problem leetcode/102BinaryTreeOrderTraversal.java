026e00563dae91c769f2a1dc286831c2c74bf81d
harshwa007 <harshwa007@gmail.com>
2023-11-15 22:44:12 +0530

    .

--- Tree/Problem leetcode/102BinaryTreeOrderTraversal.java
+++ Tree/Problem leetcode/102BinaryTreeOrderTraversal.java
@@ -27,3 +27,35 @@ class Solution {
         return ll;
     }
 }
+/*
+ * just reverse order of levels
+ * class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ll=new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null)
+        {
+            return ll;
+        }
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            List<Integer> arr = new ArrayList<>();
+            int level=q.size();
+            for(int i=0;i<level;i++)
+            {
+                TreeNode curr=q.poll();
+                arr.add(curr.val);
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+
+            }
+            ll.add(arr);
+        }
+        Collections.reverse(ll);
+        return ll;
+    }
+}
+ * 
+ * 
+ */
