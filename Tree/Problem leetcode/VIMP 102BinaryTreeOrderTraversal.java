4198f8fecc39fa99355aaf5c63a337bf2dc3a7f9
Harshvardhan Wadavane <71274010+harshwa007@users.noreply.github.com>
2026-06-10 22:46:23 +0530

    Update and rename 102BinaryTreeOrderTraversal.java to VIMP 102BinaryTreeOrderTraversal.java

--- Tree/Problem leetcode/VIMP 102BinaryTreeOrderTraversal.java
+++ Tree/Problem leetcode/VIMP 102BinaryTreeOrderTraversal.java
@@ -1,3 +1,64 @@
+102. Binary Tree Level Order Traversal
+Hint
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+
+--------------------------------------------------------------------------------------
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null)
+        {
+            return list;
+        }
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            List<Integer> temp = new ArrayList<>();
+            int size = q.size();
+            for(int i = 0; i< size; ++i)
+            {
+                TreeNode node = q.poll();
+                temp.add(node.val);
+                if(node.left != null)
+                {
+                    q.offer(node.left);
+                }
+                if(node.right != null)
+                {
+                    q.offer(node.right);
+                }
+            }
+            list.add(temp);
+        }
+        return list;
+    }
+}
+---------------------------------------------------------------------------------------    
+
 package Tree.Problem leetcode;
 
 public class 102BinaryTreeOrderTraversal {
